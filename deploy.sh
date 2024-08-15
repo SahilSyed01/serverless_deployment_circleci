8bedf848dae1373a23b9f6d57c888b8a075b35a6
SahilSyed01 <sahilsyed7781@gmail.com>
2024-08-15 20:21:28 +0530

    C

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,36 @@
-#!/bin/bash
+version: 2.1
 
-# Copy the binary to the EC2 instance
-scp -i $EC2_SSH_KEY -o StrictHostKeyChecking=no /workspace/myservice ec2-user@$EC2_PUBLIC_IP:/home/ec2-user/myservice
+jobs:
+  build:
+    docker:
+      - image: circleci/golang:1.22  # Update to a valid tag
+    steps:
+      - checkout
+      - run:
+          name: Build Go Binary
+          command: |
+            go build -o myservice ./path/to/your/main.go
+      - persist_to_workspace:
+          root: .
+          paths:
+            - myservice
 
-# Execute commands on the EC2 instance
-ssh -i $EC2_SSH_KEY -o StrictHostKeyChecking=no ec2-user@$EC2_PUBLIC_IP << 'ENDSSH'
-  sudo mv /home/ec2-user/myservice /usr/local/bin/myservice
-  sudo systemctl restart myservice || (sudo systemctl start myservice)
-ENDSSH
+  deploy:
+    docker:
+      - image: circleci/python:3.8
+    steps:
+      - attach_workspace:
+          at: /workspace
+      - run:
+          name: Deploy to AWS EC2
+          command: |
+            ./deploy.sh
+
+workflows:
+  version: 2
+  deploy:
+    jobs:
+      - build
+      - deploy:
+          requires:
+            - build
